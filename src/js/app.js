40b37af1c38055b7136deb5e641bdca560d11e4c
Konstantin Beloglazov <ajoq@ya.ru>
2023-01-11 16:41:48 +0300

    Callback Chat done

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,9 @@
 import Collapse from './Collapse';
+import ChatWidget from './ChatWidget';
 
 const collapse = new Collapse();
 collapse.init();
+
+const divWidget = document.querySelector('.chat-widget');
+const chatWidget = new ChatWidget(divWidget);
+chatWidget.init();
